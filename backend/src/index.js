f6988a65bb4224ff326cae1f401bd5a426f7c577
Abhishek Prajapati <worktoabhishek@gmail.com>
2026-04-25 15:14:03 +0530

    change in database

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,6 +12,8 @@ import roomRoutes from "./routes/roomRoutes.js";
 // 1. Initialize Express and HTTP Server
 const app = express();
 const httpServer = createServer(app);
+const PORT = process.env.PORT || 5000;
+const CLIENT_URL= process.env.CLIENT_URL;
 
 // 2. Setup Socket.IO with CORS
 const io = new Server(httpServer, {
